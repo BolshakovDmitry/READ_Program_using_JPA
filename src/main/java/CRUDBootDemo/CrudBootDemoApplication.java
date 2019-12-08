67f264dbcff1920d9c9f2b6a7e2088a6802a0744
bolshakoff.dima@yandex.ru <2011501099Dmitry>
2019-12-08 19:12:55 +0300

    Initial commit

---
+++ src/main/java/CRUDBootDemo/CrudBootDemoApplication.java
@@ -0,0 +1,13 @@
+package CRUDBootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBootDemoApplication.class, args);
+	}
+
+}
